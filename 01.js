d4a2139c2f44d3684435998da4af799cf50cc66f
SanjeevCod3r <Sanjeev1822.be21@chitkara.edu.in>
2023-01-24 17:00:01 +0530

    Frist Commit

---
+++ 01.js
@@ -0,0 +1,11 @@
+console.log("Program To Add Three Number")
+
+// Program To Add Three Number
+
+    function addThreeNumber(a, b, c){
+        return a + b + c;
+    }
+
+    let C = addThreeNumber(3, 5, 6)
+        console.log(C)
+    
