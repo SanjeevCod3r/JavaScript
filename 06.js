daacf43ae819ffad516d6f3bafe3d392175c9c73
SanjeevCod3r <Sanjeev1822.be21@chitkara.edu.in>
2023-01-24 19:57:07 +0530

    Day 6 Done

---
+++ 06.js
@@ -0,0 +1,55 @@
+// Operator In Js 
+
+
+
+// Arthmatic Operators
+let a = 45;
+let b = 4;
+console.log(a+b)
+console.log(a-b)
+console.log(a/b)
+console.log(a*b)
+console.log(a**b)
+console.log(a%b)
+console.log(a++)
+console.log(a--)
+console.log(--a)
+console.log(++a)
+
+//Assignment Operator
+let c = 10;
+
+c += 5  // Same as c = c + 5
+console.log(c)
+
+//  =
+//  +=
+//  -=
+//  *=
+//  /=
+//  %=
+//  **=
+
+
+
+
+//   Comparison Operators
+
+//   ==  ---> Equal To
+//   !=  ---> Not Equal
+//   ===  ---> equal Value And Type
+//   !==  ---> not equal value or not equal type
+//   >    ---> Greater than
+//   <    ---> Less Than
+//   >=   ---> Grater Than Or Equal to
+//   <=   ---> less Than or Equal to
+//   ?    ---> Turanary Operator 
+
+
+//  Logical Operator
+
+//  && --->  and 
+//  || --->  or
+//  ! --->   not    
+
+
