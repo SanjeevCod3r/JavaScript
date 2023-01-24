052c793878e05e8983fb39292c2be4b4ce835541
SanjeevCod3r <Sanjeev1822.be21@chitkara.edu.in>
2023-01-24 17:43:15 +0530

    Day 3 Done

---
+++ 03.js
@@ -0,0 +1,19 @@
+// Diff b/w let, var, Const
+
+// Var -> Var Is Globally Scoped , 
+// Var Can Be Update & Re-Declared Within its Scope
+
+
+// Let Can Be Update but Not Re-Declared
+
+
+// Const can Neither be Update nor Be Re-Declared 
+
+
+//..................................................//
+
+const author = "Sanjeev"
+// author = 6 -------->  Throw An Error Because Const Can't Be Changed 
+
+
+    
