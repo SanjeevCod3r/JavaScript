98d4cc171048d41bc4d0889f4f16cb605e373d4d
SanjeevCod3r <Sanjeev1822.be21@chitkara.edu.in>
2023-01-24 19:24:44 +0530

    Day 5 Done

---
+++ 05_PracticeSet.js
@@ -0,0 +1,40 @@
+// Create a veriable of type string try to add a number to it
+
+let a = "Sanjeev"
+let b = "6"
+console.log(a + b)  
+
+// Use Typeof Operator to find the DataType of a String in last question
+
+console.log(typeof (a+b))
+
+// Create a Const Object in JavaScript Can u Change it to Hold a number Later
+
+const d = {
+    name: "Sanjeev",
+    section: 6,
+    isPrincipal: false
+}
+ //   d = 54
+
+ // ANS ---> No
+
+ // Try to add a new key to const Object in Problem 3 Where You able to do it
+
+ d['friend'] = "Shanu"
+ d['name'] = "Jaiswal"
+console.log(d)
+
+
+
+
+// Write a Js Program to Create a Word-Meaning Dictonary Of 5 Words
+
+const dist = {
+    Apollo: "a bronze statue of Apollo",
+    apoplexy: "very great anger",
+    apparel: "clothes of a particular type when they are being sold in a shop"
+}
+
+console.log(dist.apparel)
+
