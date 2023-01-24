161aec5734b5a201b9b6ced5d2eee3ac64ef1825
SanjeevCod3r <Sanjeev1822.be21@chitkara.edu.in>
2023-01-24 17:21:06 +0530

    Day 2 Done

---
+++ 02.js
@@ -0,0 +1,13 @@
+let a = 67; // a contain 67
+console.log(a)
+
+
+a = "Sanjeev"
+console.log(a)
+
+// let 8sanjeev = 7 ---------> // Not allowed this will throw an error 
+
+// let var = 7 ----------> Reserved Keyword In Javascript
+
+// JavaScript Is Case Sensitive 
+
