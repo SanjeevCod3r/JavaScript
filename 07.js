6cd38ddaf3d2d71649997b3b098a1cdadffb194d
SanjeevCod3r <Sanjeev1822.be21@chitkara.edu.in>
2023-01-29 18:23:52 +0530

    All Done

---
+++ 07.js
@@ -0,0 +1,17 @@
+// Conditional Expression
+
+
+// If Statement
+
+
+ let a = prompt("What Is Your Age ?")
+ a = Number.parseInt(a) // Coverting The String to a Number 
+ if(a>0){
+    alert("This Is Valid Age")
+ }
+
+
+ // If Else Statement 
+
+// Switch Case
+
---
+++ 08_PracticeSet.js
@@ -0,0 +1,11 @@
+// Lie b/w 10 and 20 --> Problem No. 1
+
+let age = prompt("Enter your Age :")
+
+if(age>10 && age<20){
+    console.log("Your Age Lie Between 10 and 20")
+}
+else{
+    console.log("Do Not Lie")
+
+}
