1b5037c186be0428fe55e49428a88c8fe655d7ba
SanjeevCod3r <Sanjeev1822.be21@chitkara.edu.in>
2023-01-30 01:24:06 +0530

    Day 8 Done

--- 10.js
+++ 10.js
@@ -1,7 +1,21 @@
 // while loop
+
+/*
 let a = prompt("Enter The Value Of N")
 n = Number.parseInt(n)
 while(i<n){
     console.log(n)
     i++;
 }
+*/
+
+// Do While
+
+let a = prompt("Enter The Value Of N")
+n = Number.parseInt(n)
+let i = 0;
+do{
+    console.log(i)
+    i++;
+}
+while(i<n)
